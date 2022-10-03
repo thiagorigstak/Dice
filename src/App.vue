18cf633333afbd13ea49e96a20b46a26e9990735
thiago.stachuk <thiago.stachuk@caubr.gov.br>
2022-10-03 11:43:36 -0300

    melhora de design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <button @click="roll">roll</button>
-    <p> Resultado: {{dice}} </p>
-    <div v-if="dice != 0">
-      <img :src="selected"/>
+    <div class="container-fluid">
+      <H1>
+        <button type="button" class="btn btn-dark btn-lg" @click="roll">🎲 roll 🎲</button>
+      </H1>
+      <div v-if=" dice1 > dice2">
+        <p> 🚩 Player 1 Ganhou </p>
+      </div>
+      <div v-else-if=" dice2 > dice1">
+        <p> Player 2 Ganhou 🚩</p>
+      </div>
+      <div v-else>
+        <p>🎲 Empate 🎲</p>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <img :src="selected1"/>
+        </div>
+        <div class="col-sm">
+          <img :src="selected2"/>
+        </div>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -20,18 +35,39 @@
           'dice5.png',
           'dice6.png',
         ],
-        selected: '',
-        dice: 0,
+        selected1: '',
+        selected2: '',
+        dice1: 0,
+        dice2: 0,
       }
     },
     created(){
-      this.selected = this.img[1];
+      this.selected1 = this.img[0];
+      this.selected2 = this.img[0];
+      this.roll();
     },methods:{
       roll: function(){
-      this.dice = Math.ceil(Math.random() *6);
-      this.selected = new URL('./components/icons/'+ this.img[this.dice-1], import.meta.url).href;
-      console.log(this.selected);
+      this.dice1 = Math.ceil(Math.random() *6);
+      this.dice2 = Math.ceil(Math.random() *6);
+      this.selected1 = new URL('./components/icons/'+ this.img[this.dice1-1], import.meta.url).href;
+      this.selected2 = new URL('./components/icons/'+ this.img[this.dice2-1], import.meta.url).href;
+      console.log(this.selected1, this.selected2);
       }
     }
   }
 </script>
+
+<style>
+  p {
+    font-size: 2rem;
+    color: #4ECCA3;
+    font-family: 'Indie Flower', cursive;
+  }
+  h1 {
+    margin: 30px;
+    font-family: 'Lobster', cursive;
+    text-shadow: 5px 0 #232931;
+    font-size: 8rem;
+    color: #4ECCA3;
+  }
+</style>
